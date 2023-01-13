27e404358d2afefc3aadad5ea61822f54d07f0de
Ren Roetzel <renroetzel@gmail.com>
2023-01-13 10:20:56 -0500

    delete arrow functions

--- ES2015-arrow-func-exercise.js
+++
@@ -1,7 +0,0 @@
-const double = (arr) => arr.map(val => val * 2);
-
-const squareAndFindEvens = (numbers) => {
-    const squares = numbers.map(num => num ** 2);
-    const evens = squares.filter(square => square % 2 === 0);
-    return evens;
-};
